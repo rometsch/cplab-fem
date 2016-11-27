94099f43b9ad75e2f2b086e69b0c1b508be41b5a
Thomas Rometsch <thomas.rometsch@gmail.com>
2016-11-27 20:32:29 +0100

    change script to function to be called with a grid file and adjust plot function to produce a 3x3 subplot figure of the eigenfunction to the smallest nine eigenvalues

---
+++ fem_solve_grid.m
@@ -0,0 +1,53 @@
+function [] = fem_solve_grid(filename)
+%FEN_SOLVE_GRID L?st das Eigenwertproblem auf der in filename gegebenen
+%Geometrie und stellt die Eigenfunctionen zu den kleinsten 9 Eigenwerten
+%graphisch dar.
+    % Definiere Parameter
+    N_eigenvals = 9;
+
+
+    % Gitter laden
+    [tri, x, y, N_elem, N_pts] = readData(filename);
+
+    % Graphische Darstellung der Gitter
+    % plotGrid(tri,x,y);
+
+    % Berechnung der Matrizen
+    n = 1:N_elem;
+    g = ( x(tri(n,2)) - x(tri(n,1)) ).*( y(tri(n,3)) - y(tri(n,1)) ) - ( x(tri(n,3)) - x(tri(n,1)) ).*( y(tri(n,2)) - y(tri(n,1)) );
+
+    d = 1/6*reshape(repmat(g,3,1),1,3*N_elem);
+    D = sparse(1:3*N_elem,1:3*N_elem,d);
+
+    K = sparse([],[],[],3*N_elem,3*N_elem,9*N_elem);
+
+    for n = 1:N_elem
+       X = [x(tri(n,1:3)); y(tri(n,1:3)); [1,1,1]];
+       A = (X)^(-1);
+       K_n = zeros(3,3);
+       for k = 1:3
+           for l = 1:3
+               K_n(k,l) = A(k,1)*A(l,1) + A(k,2)*A(l,2);
+           end
+       end
+       ind = n*3-2:n*3;
+       K(ind,ind) = -0.5*g(n)*K_n;
+    end
+
+    T = sparse(N_pts,3*N_elem);
+    for i = 1:N_elem
+        for k = 1:3
+           T(tri(i,k),3*(i-1)+k) = 1;
+        end
+    end
+
+    % Berechne die Eigenwerte (und Eigenvektoren)
+    [EigVec,EigVal] = eigs(T*K*T.',T*D*T.',N_eigenvals,'sm');
+
+    % Stelle Eigenfunktionen graphisch dar
+    plotGridEig(tri,x,y,EigVec,diag(EigVal));
+
+    
+    % Eigenwerte augeben
+    disp(diag(EigVal));
+end
--- plotGridEig.m
+++ plotGridEig.m
@@ -1,17 +1,23 @@
-function [ fig ] = plotGridEig( tri, x, y, EigVec, EigVal, n)
+function [ fig ] = plotGridEig( tri, x, y, EigVec, EigVal)
 %PLOTGRID Plot all elements of a triangularization
 % Points are given by X and Y coordinates and the elements by tri.
-% Eigenvector Matrix is given by EigVec and Figures are plotted for the n
+% Eigenvector Matrix is given by EigVec and Figures are plotted for the 9
 % first eigenvalues.
-    for i = 1:n
-        fig = figure();
-        daspect([1 1 1]);
+    fig = figure();
+    daspect([1 1 1]);
+    for i = 1:9
+        subplot(3,3,i);
         Vec = EigVec(:,i);
-        fill( x(tri).', y(tri).', Vec(tri).' );
-        title(sprintf('%s%f','\lambda = ',EigVal(i)));
+        h = fill( x(tri).', y(tri).', Vec(tri).' );
+        title(sprintf('%s%0.3f','\lambda = ',EigVal(i)));
         xlabel('x-Richtung');
         ylabel('y-Richtung');
-        colorbar
+        cb=colorbar;
+        set(cb,'yticklabel',num2str(str2double(get(cb,'yticklabel')),'%0.2f'))
+        colormap('jet');
+        set(h,'EdgeColor','None');
+        axis([-inf inf -inf inf])
     end
+    set(fig,'PaperSize',[9 6]);
 end
 
